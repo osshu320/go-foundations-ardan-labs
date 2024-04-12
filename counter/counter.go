42247278e0eee613ce024d51314ce088a910752f
shubhamchemate003 <shubhamchemate3@gmail.com>
2024-04-12 23:01:31 +0530

    Day03 Part03

---
+++ counter/counter.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	// solution 1: use mutex
+	/*
+		var mu sync.Mutex
+		count := 0
+	*/
+	// count := int64(0)
+	var count int64
+
+	const n = 10
+	var wg sync.WaitGroup
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 10_000; j++ {
+				/*
+					mu.Lock()
+					count++
+					mu.Unlock()
+				*/
+				atomic.AddInt64(&count, 1)
+			}
+		}()
+	}
+	wg.Wait()
+
+	log.Println(count)
+}
