2f22efd44c82bf5fc5e5151a13bed03cb537bc7f
shubhamchemate003 <shubhamchemate3@gmail.com>
2024-05-09 20:29:18 +0530

    slices  revisit

--- revisit/revisit.go
+++ revisit/revisit.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"sort"
 	"strings"
 	"time"
 	"unicode/utf8"
@@ -96,6 +97,86 @@ func sha1Sum(fileName string) (string, error) {
 
 	sig := w.Sum(nil)
 	return fmt.Sprintf("%x", sig), nil
+}
+
+func appendInt(s []int, v int) []int {
+	i := len(s)
+
+	if len(s) < cap(s) {
+		s = s[:len(s)+1]
+	} else {
+		fmt.Printf("reallocate: %d->%d\n", len(s), 2*len(s)+1)
+		s2 := make([]int, 2*len(s)+1)
+		copy(s2, s)
+		s = s2[:len(s)+1]
+	}
+
+	s[i] = v
+	return s
+}
+
+func concat(s1 []string, s2 []string) []string {
+	s := make([]string, len(s1)+len(s2))
+	copy(s[:len(s1)], s1)
+	copy(s[len(s1):], s2)
+	return s
+
+	// return append(s1, s2...)
+}
+
+func median(vs []float64) (float64, error) {
+	if len(vs) == 0 {
+		return 0, fmt.Errorf("median of empty slice")
+	}
+
+	nums := make([]float64, len(vs))
+	copy(nums, vs)
+
+	sort.Float64s(nums)
+	n := len(nums)
+	if n%2 == 1 {
+		return nums[n/2], nil
+	}
+
+	return (nums[n/2] + nums[n/2-1]) / 2, nil
+}
+
+func slices_go() {
+	var s []int
+	fmt.Println("len", len(s))
+	if s == nil {
+		fmt.Println("nil slice")
+	}
+
+	s2 := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Printf("s2 = %#v\n", s2)
+
+	s3 := s2[1:4]
+	fmt.Printf("s2 = %#v\n", s3)
+
+	s3 = append(s3, 100)
+	fmt.Printf("s3 (append) = %#v\n", s3)
+	fmt.Printf("s2 (append) = %#v\n", s2)
+	fmt.Printf("s2: len=%d, cap=%d\n", len(s2), cap(s2))
+	fmt.Printf("s3: len=%d, cap=%d\n", len(s3), cap(s3))
+
+	var s4 []int
+	for i := 0; i < 1000; i++ {
+		s4 = appendInt(s4, i)
+	}
+	fmt.Println("s4", len(s4), cap(s4))
+
+	// s4[1001] = 5 // panic
+
+	fmt.Println(concat([]string{"A", "B"}, []string{"C", "D", "E"}))
+
+	vs := []float64{2, 1, 3}
+	fmt.Println(median(vs))
+	vs = []float64{2, 1, 3, 4}
+	fmt.Println(median(vs))
+	fmt.Println(vs)
+
+	fmt.Println(median(nil))
 }
 
 func sha1_go() {
