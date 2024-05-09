e0f907eb32545c90f9b0329ab09b1b97895f78b8
shubhamchemate003 <shubhamchemate3@gmail.com>
2024-05-09 21:02:33 +0530

    game revisit

--- revisit/revisit.go
+++ revisit/revisit.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"slices"
 	"sort"
 	"strings"
 	"time"
@@ -139,6 +140,146 @@ func median(vs []float64) (float64, error) {
 	}
 
 	return (nums[n/2] + nums[n/2-1]) / 2, nil
+}
+
+const (
+	Jade Key = iota + 1
+	Copper
+	Crystal
+	invalidKey // internal - not exported
+)
+
+const (
+	maxX = 1000
+	maxY = 600
+)
+
+type Key byte
+
+type Item struct {
+	X int
+	Y int
+}
+
+func NewItem(x, y int) (*Item, error) {
+	if x < 0 || x > maxX || y < 0 || y > maxY {
+		return nil, fmt.Errorf("%d/%d out of bounds %d/%d", x, y, maxX, maxY)
+	}
+
+	i := Item{
+		X: x,
+		Y: y,
+	}
+
+	// Go compiler does escape analysis and allocate i on heap
+	return &i, nil
+}
+
+// i is receiver
+// if you want to mutate use pointer receiver
+func (i *Item) Move(x, y int) {
+	i.X = x
+	i.Y = y
+}
+
+type Player struct {
+	Name string
+	Item
+	Keys []Key
+}
+
+func (p *Player) FoundKey(k Key) error {
+	if k < Jade || k >= invalidKey {
+		return fmt.Errorf("invalid key: %#v", k)
+	}
+
+	// if !containsKey(p.Key, k) {
+	if !slices.Contains(p.Keys, k) {
+		p.Keys = append(p.Keys, k)
+	}
+
+	return nil
+}
+
+func containsKey(keys []Key, k Key) bool {
+	for _, kk := range keys {
+		if kk == k {
+			return true
+		}
+	}
+	return false
+}
+
+type mover interface {
+	Move(x, y int)
+}
+
+// Rule of Thumb: Accept interfaces, return types
+func moveAll(ms []mover, x, y int) {
+	for _, m := range ms {
+		m.Move(x, y)
+	}
+}
+
+func (k Key) String() string {
+	switch k {
+	case Jade:
+		return "jade"
+	case Copper:
+		return "copper"
+	case Crystal:
+		return "crystal"
+	}
+
+	return fmt.Sprintf("<Key %d>", k)
+}
+
+func game_go() {
+	var i1 Item
+	fmt.Println(i1)
+	fmt.Printf("i1: %#v\n", i1)
+
+	i2 := Item{1, 2}
+	fmt.Printf("i2: %#v\n", i2)
+
+	i3 := Item{
+		Y: 10,
+	}
+	fmt.Printf("i3: %#v\n", i3)
+	fmt.Println(NewItem(10, 20))
+	fmt.Println(NewItem(10, -20))
+
+	i3.Move(100, 200)
+	fmt.Printf("i3 (move): %#v\n", i3)
+
+	p1 := Player{
+		Name: "Parzival",
+		Item: Item{500, 300},
+	}
+	fmt.Printf("p1: %#v\n", p1)
+	fmt.Printf("p1.X: %#v\n", p1.X)
+	fmt.Printf("p1.Item.X: %#v\n", p1.Item.X)
+	p1.Move(400, 500)
+	fmt.Printf("p1 (move): %#v", p1)
+
+	ms := []mover{
+		&i1,
+		&p1,
+		&i2,
+	}
+	moveAll(ms, 0, 0)
+	for _, m := range ms {
+		fmt.Println(m)
+	}
+
+	k := Jade
+	fmt.Println("k:", k)
+	fmt.Println("key:", Key(17))
+
+	p1.FoundKey(Jade)
+	fmt.Println(p1.Keys)
+	p1.FoundKey(Jade)
+	fmt.Println(p1.Keys)
 }
 
 func slices_go() {
