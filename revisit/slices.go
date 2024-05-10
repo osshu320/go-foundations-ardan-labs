9327eb464a2c97f133c175431399cba962da8bb7
shubhamchemate003 <shubhamchemate3@gmail.com>
2024-05-10 20:48:40 +0530

    slices

--- revisit/slices.go
+++ revisit/slices.go
@@ -7,10 +7,24 @@ func SubOneFromLen(slice []int) []int {
 	return slice
 }
 
+type path []byte
+
+func (p *path) ToUpper() {
+	for i, b := range *p {
+		if 'a' <= b && b <= 'z' {
+			(*p)[i] = b + 'A' - 'a'
+		}
+	}
+}
+
 func slices_go_main() {
 	a := []int{1, 2, 3, 4, 5}
 	fmt.Println("a", len(a), a)
 	b := SubOneFromLen(a)
 	fmt.Println("a", len(a), a)
 	fmt.Println("b", len(b), b)
+
+	pathName := path("/usr/bin/tso©")
+	pathName.ToUpper()
+	fmt.Printf("%s\n", pathName)
 }
