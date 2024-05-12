1b99d79c4a4d0feeb2adfeadeacd1c666a271f29
shubhamchemate003 <shubhamchemate3@gmail.com>
2024-05-12 11:43:00 +0530

    slices examples

--- extras/slices.go
+++ extras/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func SubOneFromLen(slice []int) []int {
 	slice = slice[0 : len(slice)-1]
@@ -17,6 +20,14 @@ func (p *path) ToUpper() {
 	}
 }
 
+func appendSquares(p []int) {
+	n := len(p)
+	for i := 0; i < n; i++ {
+		p = append(p, p[i]*p[i])
+	}
+	log.Println("in appendSquares:", p, len(p), cap(p))
+}
+
 func slices_go_main() {
 	a := []int{1, 2, 3, 4, 5}
 	fmt.Println("a", len(a), a)
@@ -27,4 +38,10 @@ func slices_go_main() {
 	pathName := path("/usr/bin/tso©")
 	pathName.ToUpper()
 	fmt.Printf("%s\n", pathName)
+
+	p := []int{1, 2, 3, 4}
+
+	log.Println("in slices_go_main:", p, len(p), cap(p))
+	appendSquares(p)
+	log.Println("in slices_go_main:", p, len(p), cap(p))
 }
